e46075369305c411b8f1fa434c65bfeb222e3f9b
Svetlana Berdnikova <sveta0811b@mail.ru>
2026-05-08 15:23:36 +0300

    Modal sucess/error, btn open/clode model, swiper

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+
+//Open - close modal-windows
+
+function modalToggle(elem, displayValue) {
+    elem.style.display = displayValue
+}
+
+const btnOpenModal = document.querySelectorAll('[data-show-modal]');
+const modalWindow = document.querySelector('[data-modal]');
+
+for (let button of btnOpenModal) {
+    button.addEventListener('click', () => {
+        modalToggle(modalWindow, 'block');
+    });
+}
+
+const crossCloseModal = document.querySelectorAll('[data-close-modal]')
+
+for (let close of crossCloseModal) {
+    close.addEventListener('click', (event) => {
+        const modal = event.target.closest ('[data-modal]')
+        modalToggle(modal, 'none')
+    })
+}
+
+//Slider-reviews
+
+const reviewsSlider = new Swiper ( ".reviews", {
+    navigation: {
+        prevEl: ".reviews__nav-prev",
+        nextEl: ".reviews__nav-next",
+    },
+
+    simulateTouch: true,
+    touchRatio:1,
+    touchAngle:45,
+    grabCursor:true,
+    loop: true,
+    slidesPerView: 1,
+spaceBetween:90,
+
+    breakpoints: {
+
+        // 1440: {spaceBetween: 120},
+        // 768: {spaceBetween: 100},
+        // 540: {spaceBetween: 50},
+    },
+
+    // autoplay: {
+    //     delay: 3000,
+    // },
+});
